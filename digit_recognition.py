1b54722205504e276661a5af5f14abf5d3eaebca
raisonm <mattandthings@gmail.com>
2017-04-22 16:30:52 -0400

    this now optimizes over all the cases passed in and calculated the percentage correct when iterating through all of the results again as a "fake" final run

--- digit_recognition.py
+++ digit_recognition.py
@@ -21,16 +21,14 @@ def normalize_expected_digit_array(d, expect):
 		return 0
 
 array_normalize_expected_digit_array = numpy.vectorize(normalize_expected_digit_array,  otypes=[numpy.float])
+array_f = numpy.vectorize(sigmoid, otypes=[numpy.float]); # why a numpy float???
+array_df = numpy.vectorize(d_sigmoid,  otypes=[numpy.float])
 
 # training_set = csv.reader(open("train.csv"), delimiter=",");
 training_set = numpy.genfromtxt("train.csv", delimiter=',');
 
 is_training = 0;
-# the first row is just going to be the labels for the columns...so don't really do anything with them at this point.
-# first_row = next(training_set);
 training_set = training_set[1:,:] # get rid of labels...
-
-digit_range = [0,1,2,3,4,5,6,7,8,9];
 
 
 ######
@@ -40,13 +38,6 @@ digit_range = [0,1,2,3,4,5,6,7,8,9];
 # I don't think it matters what weights we start with so we'll start with just random numbers between 0 and 1
 # To help normalize values I'll devide each x input by 255, the max value, so we start of with a similar sort of value between 0 and 1.
 ######
-
-x_inputes = numpy.array([[]]);
-
-w1 = numpy.array([[]]);
-w2 = numpy.array([[]]);
-
-p = 1;
 
 if is_training == 0:
 	# input_set = 2;
@@ -73,25 +64,34 @@ if is_training == 0:
 	w1 = numpy.genfromtxt("w1_values.csv", delimiter=",");
 	# there will be only one output node. there will also be n inputs to this one node as well.
 	w2 = numpy.genfromtxt("w2_values.csv", delimiter=",");
-	array_f = numpy.vectorize(sigmoid, otypes=[numpy.float]); # why a numpy float???
 
-	y1 = numpy.dot(x_inputes, w1);
-	z1 = array_f(y1);
-	y2 = numpy.dot(z1, w2);
-	z2 = array_f(y2);
+	successes = 0 
+	failures = 0
+	i = 0
+	print 'iteration cap: ' + str(x_inputes.shape[0])
+	while i < x_inputes.shape[0]:
+		x = x_inputes[i,:][numpy.newaxis] ## this will be the value that changes each time. we'll iterate through all the different sets of x inputs
+		actual = d[i,:][numpy.newaxis]
 
-	diff = d - z2;
-	# print 'lets see how many are correct. where ever there is a 1 there\'s an error...'
-	dist = numpy.absolute(diff);
-	# print ''
-	errors = numpy.count_nonzero(dist)
-	total = dist.shape[0] * dist.shape[1]
-	success = total - errors
-	ratio = (success * 1.0) / (total * 1.0)
+		y1 = numpy.dot(x, w1);
+		z1 = array_f(y1);
+		y2 = numpy.dot(z1, w2);
+		z2 = array_f(y2);
+
+		diff = actual - z2;
+		# print 'lets see how many are correct. where ever there is a 1 there\'s an error...'
+		dist = numpy.absolute(diff);
+		errors = numpy.count_nonzero(dist)
+		failures += errors
+		print 'iteration: ' + str(i);
+		i += 1
+	
+
+	successes = d.shape[0]*d.shape[1] - failures
+	ratio = (successes*1.0) / (d.shape[0]*d.shape[1] * 1.0)
 	print ''
-	print 'total cells: ' + str(total)
-	print 'number of errors: ' + str(errors)
-	print 'number of successes:' + str(success)
+	print 'number of errors: ' + str(failures)
+	print 'number of successes:' + str(successes)
 	print 'percentage correct:' + str(ratio)
 	# numpy.set_printoptions(threshold='nan');
 
@@ -104,15 +104,9 @@ if is_training == 0:
 	quit();
 
 
-step_size = 1
-
-# first_row = training_set.shape[1] ## this should get the number of columns, or rather then umber of x inputs each run
-
-array_f = numpy.vectorize(sigmoid, otypes=[numpy.float]); # why a numpy float???
-array_df = numpy.vectorize(d_sigmoid,  otypes=[numpy.float])
+step_size = 1.0
 
 x_inputes = numpy.array(training_set).astype(float);
-
 d = x_inputes[:,0][numpy.newaxis].T
 generator_d = d;
 
@@ -134,7 +128,8 @@ d = array_normalize_expected_digit_array(d, array_of_possible_solutions.shape[1]
 
 x_inputes = x_inputes[:,1:] ## ignore first column. thats the one with the solutions.
 x_inputes = x_inputes * (0.0039)
-x_inputes = x_inputes - 0.5 ## scale it back some
+# x_inputes = x_inputes - 0.5 ## scale it back some
+x_inputes = x_inputes / x_inputes.shape[1]
 
 
 ## initiate the values here
@@ -142,170 +137,86 @@ m = x_inputes.shape[1]; # number of hidden nodes
 s1 = m;
 w1 = numpy.random.uniform(-1.0, 1.0, (m, s1)); ## I THINK THIS IS THE ORDER BUT I SHOULD DOUBLE CHECK
 s2 = d.shape[1] ## we'll need 10 different synapses on our last layer to generate all the solutions.
-w2 = numpy.random.uniform(-1.0, 1.0, (s1, s2)); ## here we get a 42000X10 matrix
-dp_dz = numpy.ones((1, d.shape[1]));
+w2 = numpy.random.uniform(-1.0, 1.0, (s1, s2)); # here we get a 42000X10 matrix
+## previously  was 1, d.shape[1]
+dp_dz = numpy.ones((d.shape[0], d.shape[1]));
 
-
+cumulative_product = 1;
 i = 0;
-while numpy.amax(dp_dz) > 0.05 and i < 10:
+## cumulative_product > 0.0005 and
+while i < 50:
+	cumulative_product = numpy.prod(numpy.absolute(dp_dz));
 	print "iteration number: " + str(i);
-	i += 1
+	## ok ok I think I got it....use all the inputs to start so that those end up changing the w1 and w2 values...yes?
+	## it'll optimize for all of the input sets and stuff...maybe
 
-	y1 = numpy.dot(x_inputes, w1);
-	z1 = array_f(y1);
+	## 1Xm array
+	# x = x_inputes[i,:][numpy.newaxis] ## this will be the value that changes each time. we'll iterate through all the different sets of x inputs
+	# actual = d[i,:][numpy.newaxis]
+	x=x_inputes
+	actual=d
+
+	## nXs1 array
+	y1 = numpy.dot(x, w1);
+	## nXs1 array
+	z1 = array_f(y1); ## at this point we've obtained the activation on all hidden layer synapses
+	## nXs2 array
 	y2 = numpy.dot(z1, w2);
-	z2 = array_f(y2);
-	print z2.shape
-	print '------'
-	print d.shape
-	dp_dz = d - z2 # at this point this will be 42000X10
-	# print "error is: " + str(dp_dz);
-	# print dp_dz.shape
-	# print numpy.amax(dp_dz)
+	## nXs2 array
+	z2 = array_f(y2); ## at this point we've obtained the activation on all final layer synapses
+	# print z2.shape
+	# print '------'
+	# print actual.shape
+	dp_dz = actual - z2 # should be a 1Xs2 array
 
 	## The only reason this isn't a dot product is because the activation on the final layer synapse will be a single value for each system.
 	## a new model will need to be consider for systems where the final layer has more than one synapse
 	delta_w2 = dp_dz * array_df(z2);
-	print '--------------'
-	print 'dp_dz'
-	print dp_dz.shape
-	print '--------------'
-	print 'z2'
-	print z2.shape
-	print '--------------'
-	print 'delta_w2'
-	print delta_w2.shape
+	# print '--------------'
+	# print 'dp_dz'
+	# print dp_dz.shape
+	# print '--------------'
+	# print 'z2'
+	# print z2.shape
+	# print '--------------'
+	# print 'delta_w2'
+	# print delta_w2.shape
 
-	# delta_1 = delta_1[0][0];
-	# print delta_1.shape;
-
+	## 1Xs1
 	delta_w1 = numpy.dot(delta_w2, w2.T)
-	print '--------------'
-	print 'delta_w1'
-	print delta_w1.shape
-	print '--------------'
+	# print '--------------'
+	# print 'delta_w1'
+	# print delta_w1.shape
+	# print '--------------'
 
-	dp_dw1 = numpy.dot(x_inputes.T, delta_w1)
+	dp_dw1 = numpy.dot(x.T, delta_w1)
 	dp_dw2 = numpy.dot(z1.T, delta_w2)
 
-	print 'dp_dw1'
-	print dp_dw1.shape
-	print '-------------'
-	print 'dp_dw2'
-	print dp_dw2.shape
-	print '-------------'
-	print 'z1'
-	print z1.shape
-	print '-------------'
-	print 'x_inputes'
-	print x_inputes.shape
-	print '-------------'
+	# print 'dp_dw1'
 	# print dp_dw1.shape
-	# print 
-	w2 += dp_dw2 * step_size ## we're going to have to double check the math here because now this seems a little funny but i'm too lazy to check it now
-	w1 += dp_dw1 * step_size
+	# print '-------------'
+	# print 'dp_dw2'
+	# print dp_dw2.shape
+	# print '-------------'
+	# print 'z1'
+	# print z1.shape
+	# print '-------------'
+	# print 'x_inputes'
+	# print x.shape
+	# print '-------------'
+	w2 += (1) * dp_dw2 * step_size ## we're going to have to double check the math here because now this seems a little funny but i'm too lazy to check it now
+	w1 += (1) * dp_dw1 * step_size
 
-	print dp_dz
+	# print 'dp_dz'
+	# print dp_dz.shape
+	# print '-------------'
+	# print dp_dz
 	## this should write write the values of w as this becomes optimized so when this is exited everything is in an actionable state
 	numpy.savetxt("w1_values.csv", numpy.asarray(w1), delimiter=",");
 	numpy.savetxt("w2_values.csv", numpy.asarray(w2), delimiter=",");
+	i += 1
 
 quit()
 
-# print first_row.size
-# quit();
 
-# while abs(p) > 0.0000000000000000000001: # we'll say this is the threshold for accurate predictions
-	# print first_row[1];
-# for i in training_set:
-
-# 	# i = next(training_set)
-# 	# d is the actual digit represented in each row....um we may want to figure out how to properly quantify this though...
-# 	# I'd suggest making it a boolean and have different branches of the neural net for each posible digit [0-9]
-# 	# that way d is either just 0 or 1; Either the sinode at the end of the net fired or it didn't.
-# 	digit_is_actually_one = i[0] == "1";
-
-# 	d1 = 0; # this value should stand for 1:yes this is a match. we should expect our neural net to calculate a match.
-	
-# 	# lets only work on those rows that are going to train for the digit "1"
-# 	if digit_is_actually_one:
-# 		d1 = 1;
-
-
-# 	# print type(i);
-# 	x_inputes = numpy.array([i]).astype(float);
-# 	x_inputes[0][0] = -1.0; ## replace the old d value with a -1. This well remove the irrelevent variable and add the threshold.
-# 	x_inputes = numpy.transpose(x_inputes);
-# 	#################################################
-# 	# run the neural net recording values for everything. We'll want to put this into its own separate function.
-
-# 	# def run_net(x, d1, ): 
-
-# 	# 1/255 = 0.0039 approximately, and we want to scale all the values to some degree to start with....I think...
-# 	# though this could be built into the w1 values, scaling now allows us to better ensure the x values are independent of eachother
-# 	x_inputes = x_inputes * (0.0039);
-
-# 	# n = x_inputes.size; # number of hidden nodes
-# 	# m = n;
-
-# 	# w1 = numpy.random.uniform(-1.0, 1.0, (m, n)); ## I THINK THIS IS THE ORDER BUT I SHOULD DOUBLE CHECK
-
-# 	# print "done";
-# 	# print x_inputes[0][0];
-
-# 	# print x_inputes[0][0];
-
-# 	# lets use y's to denote the summed values of the weights with their inpute values before they enter the sigmoid
-# 	y1 = numpy.dot(x_inputes.T, w1);
-# 	# print y1[0][0]
-
-# 	array_f = numpy.vectorize(sigmoid, otypes=[numpy.float]); # why a numpy float???
-# 	# And lets use z's to denote the value after the y's have been passed throught the sigmoid.
-# 	z1 = array_f(y1).T;
-# 	# print z1
-
-# 	# there will be only one output node. there will also be n inputs to this one node as well.
-# 	# w2 = numpy.random.uniform(-1.0, 1.0, (n, 1));
-# 	# print w2;
-# 	# print z1.size;
-
-# 	y2 = numpy.dot(z1.T, w2);
-
-# 	z2 = array_f(y2).T;
-# 	# return d1 - z2;
-
-
-# 	################################################
-# 	# now to calculate the gradient of what just happened up there...
-
-# 	array_df = numpy.vectorize(d_sigmoid,  otypes=[numpy.float])
-# # here's the issue. I use f, not f prime here.
-# 	dp_dw2 = numpy.dot(numpy.ones((1, n)), array_df(z1)) * (d1 - z2[0][0])
-
-# 	df = array_df(z1); 
-
-# 	dp_dw1 = numpy.dot(w2.T, df);
-# 	# print numpy.ones((1, n))
-# 	dp_dw1 = numpy.dot(dp_dw1, numpy.ones((1, n)));
-# 	dp_dw1 = numpy.dot(dp_dw1, x_inputes);
-# 	dp_dw1 = dp_dw1 * (d1 - z2[0][0])
-
-# 	# print dp_dw2 + dp_dw1;
-# 	p = d1 - z2;
-# 	gradient = dp_dw2 + dp_dw1;
-
-# 	w2 = w2 - (gradient * step_size);
-# 	w1 = w1 - (gradient * step_size);
-
-# 	print "p: " + str(p) + " | gradient: " + str(gradient)
-# 	# break;
-# 	# dp_dw1 = w2.T
-
-# 	# z2 is our answer for this run.
-
-
-# numpy.savetxt("w1_values.csv", numpy.asarray(w1), delimiter=",");
-# numpy.savetxt("w2_values.csv", numpy.asarray(w2), delimiter=",");
-
-# quit()
 
